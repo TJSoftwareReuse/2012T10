c6b807161013efec4b902dbcf580a68f5972f0de
yejianquan <595101714@qq.com>
2015-06-04 05:54:46 +0800

    添加测试
    添加对server3.5的测试

---
+++ src/pm_1.2/TestPM1.java
@@ -0,0 +1,22 @@
+package edu.tongji;
+import junit.framework.TestCase;
+
+public class TestPM1 extends TestCase{
+	
+	
+	
+	
+	public void testPm1(){
+		PerformanceManager p1 = new PerformanceManager("C:\\Users\\sse430-ssed\\Desktop\\", 1 , "Program1");
+		p1.outputData.run();
+		//System.out.println("未能成功输入program1的数据");
+	
+		PerformanceManager p2 = new PerformanceManager("C:\\Users\\sse430-ssed\\Desktop\\", 2 , "Program2");
+		p2.outputData.run();
+		//System.out.println("未能成功输入program2的数据");
+		
+		PerformanceManager p3 = new PerformanceManager("C:\\Users\\sse430-ssed\\Desktop\\", 3 , "Program3");
+		p3.outputData.run();
+		//System.out.println("未能成功输入program3的数据");
+	}
+}
